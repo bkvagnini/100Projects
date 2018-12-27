b0275084f996dacf8aba3125562b0b9c36270e2e
Brian K. Vagnini <brian@vagnini.net>
2018-12-26 22:05:12 -0500

    Initial Commit. Project 1000.01 Hex Dumper.

---
+++ PR02_HexDump.py
@@ -0,0 +1,10 @@
+## Hex Dumper
+## Created 12/26/18 by bkv
+## this currently doesn't work at the moment
+
+var_string = input("Enter a string to be converted to hex: ")
+input_list = list(var_string)
+ord_list = ord(input_list)
+print (ord_list)    
+#hex_string = ord(var_string)
+#print (hex_string)
